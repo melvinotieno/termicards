74fedc66572ee1698726483367badb8e30f59a17
Melvin Otieno <o.melvinotieno@gmail.com>
2023-09-21 21:33:07 +0300

    create Suit and Rank enums

---
+++ lib/enums.ts
@@ -0,0 +1,32 @@
+/**
+ * Suit is a type that represents the four suits of a standard deck of cards.
+ */
+
+export enum Suit {
+  SPADES = "♠️",
+  HEARTS = "❤️",
+  CLUBS = "♣️",
+  DIAMONDS = "♦️",
+}
+
+/**
+ * Rank is a type that represents the 13 ranks of a standard deck of cards.
+ *  - The ranks 2-10 are represented by their respective numbers.
+ *  - The ranks Jack, Queen, King, and Ace are represented by the letters J, Q, K, and A respectively.
+ */
+
+export enum Rank {
+  ACE = "A",
+  TWO = "2",
+  THREE = "3",
+  FOUR = "4",
+  FIVE = "5",
+  SIX = "6",
+  SEVEN = "7",
+  EIGHT = "8",
+  NINE = "9",
+  TEN = "10",
+  JACK = "J",
+  QUEEN = "Q",
+  KING = "K",
+}
